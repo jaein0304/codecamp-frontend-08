5025ea8da035a2b575ff5ecadf003dab4feb26cf
jaein0304 <ddc0406@naver.com>
2022-07-04 16:50:38 +0900

    init

---
+++ class/pages/01-01-qqq/aaa/index.js
@@ -0,0 +1,11 @@
+export default function Mypage() {
+    // JavaScript 쓰는 곳 
+
+    return (
+        //HTML 쓰는 곳 
+        <div>
+            안녕하세요! 
+            <input type="text"/>
+            </div>
+    )
+}
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,14 @@
+import { Wrapper, EmailInput } from '../../styles/emotion'
+export default function EmotionPage() {
+    // JavaScript 쓰는 곳 
+
+    return (
+        //HTML 쓰는 곳 
+        <Wrapper>
+            안녕하세요! 
+            <EmailInput type="text"/>
+            <EmailInput type="text"/>
+            <img src="/vercel.svg"/>
+            </Wrapper>
+    )
+}
