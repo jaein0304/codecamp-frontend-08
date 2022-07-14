f77cd11e2f1441ac3c5f7dce3d8e2ccd7ebd6819
jaein0304 <ddc0406@naver.com>
2022-07-14 13:11:26 +0900

    0714_class

---
+++ class/src/components/units/board/09-write/BoardWrite.presenter.js
@@ -0,0 +1,36 @@
+import * as S from './BoardWrite.styles'
+
+export default function BoardWriteUI(props) {
+    return (
+      <>
+        작성자 :{" "}
+        <S.RedInput
+          type="text"
+          onChange={props.onChangeWriter}
+          defaultValue={props.data?.fetchBoard.writer} //데이터가 없을 수도 있기 때문에, 옵셔널 체이닝을 해줘야 함 
+        />
+        <br />
+        제목 :{" "}
+        <input
+          type="text"
+          onChange={props.onChangeTitle}
+          defaultValue={props.data?.fetchBoard.title}
+        />
+        <br />
+        내용 :{" "}
+        <input
+          type="text"
+          onChange={props.onChangeContents}
+          defaultValue={props.data?.fetchBoard.contents}
+        />
+        <br />
+        <S.RedButton
+          qqq={props.myColor}
+          onClick={props.isEdit ? props.onClickUpdate : props.onClickCreate}
+        >
+          {props.isEdit ? "수정하기" : "등록하기"}
+        </S.RedButton>
+      </>
+    );
+}
+
