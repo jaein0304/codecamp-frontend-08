6267f356ddeba9d732465449527f949c637e00a3
jaein0304 <ddc0406@naver.com>
2022-07-25 22:23:34 +0900

    0725_댓글수정

--- freeboard_frontend/pages/boards/[boardId]/index.tsx
+++ freeboard_frontend/pages/boards/[boardId]/index.tsx
@@ -1,7 +1,8 @@
 // 상세보기 페이지
 import BoardDetail from "../../../src/components/units/board/detail/BoardDetail.container";
-import BoardCommentWrite from "../../../src/components/units/comment/write/CommentWrite.container";
-import BoardCommentList from "../../../src/components/units/comment/list/CommentList.container";
+import BoardCommentList from "../../../src/components/units/boardComment/list/BoardCommentList.container";
+import BoardCommentWrite from "../../../src/components/units/boardComment/write/BoardCommentWrite.container";
+
 export default function BoardDetailPage() {
   return (
     <>
--- freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.queries.ts
+++ freeboard_frontend/src/components/units/boardComment/write/BoardCommentWrite.queries.ts
@@ -10,10 +10,6 @@ export const CREATE_BOARD_COMMENT = gql`
       boardId: $boardId
     ) {
       _id
-      writer
-      contents
-      rating
-      createdAt
     }
   }
 `;
@@ -30,10 +26,6 @@ export const UPDATE_BOARD_COMMENT = gql`
       boardCommentId: $boardCommentId
     ) {
       _id
-      writer
-      contents
-      rating
-      updatedAt
     }
   }
 `;
