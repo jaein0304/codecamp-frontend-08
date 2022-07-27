ffc4d1865845f664e3a6a0254331f67790f6f091
jaein0304 <ddc0406@naver.com>
2022-07-27 20:49:00 +0900

    0727_backend fetchboard

---
+++ quiz_backend/Product.postgres.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn } from "typeorm"
+
+// 테이블 생성기
+@Entity()
+export class Product extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid") // https://www.tutorialspoint.com/typeorm/typeorm_entity.htm
+  _id!: string
+
+  @Column({ type: "text" })
+  seller!: string
+
+  @Column({ type: "text" })
+  name!: string
+
+  @Column({ type: "text" })
+  detail!: string
+
+  @Column({ type: "text" })
+  price!: number
+
+  @CreateDateColumn()
+  createdAt!: Date
+
+  @Column({ type: "text" })
+  deletedAt!: Date
+}
--- quiz_backend/index.ts
+++ quiz_backend/index.ts
@@ -1,5 +1,79 @@
 import { DataSource } from "typeorm"
-import { Quiz } from "./Quiz.postgres"
+import { Product } from "./Product.postgres"
+import { ApolloServer, gql } from "apollo-server"
+
+// DOCS
+const typeDefs = gql`
+  type Product {
+    _id: ID
+    seller: String
+    name: String
+    detail: String
+    price: Int
+    createdAt: String
+    deletedAt: String
+  }
+
+  input CreateProductInput {
+    name: String
+    detail: String
+    price: Int
+  }
+
+  input UpdateProductInput {
+    name: String
+    detail: String
+    price: Int
+  }
+
+  type Query {
+    fetchProducts(page: Int): [Product]
+    fetchProduct(productId: ID): [Product]
+  }
+
+  type Mutation {
+    createProduct(seller: String, createProductInput: CreateProductInput!): String
+    updateProduct(productId: ID, updateProductInput: UpdateProductInput!): String
+    deleteProduct(productId: ID): String
+  }
+`
+
+// API
+const resolvers = {
+  Query: {
+    fetchProducts: async () => {
+      const result = await Product.find()
+      console.log(result)
+      return result
+    },
+  },
+
+  Mutation: {
+    createProduct: async (_: any, args: any) => {
+      // args 는 매개변수로, 아무런 이름이나 지어도된다
+      await Product.insert({
+        seller: args.seller, // 이거해야댐!
+        ...args.createProductInput,
+        deletedAt: "",
+      })
+      return "상품 등록 성공"
+    },
+
+    updateProduct: async (_: any, args: any) => {
+      await Product.update({ _id: args.productId }, { ...args.updateProductInput, deletedAt: "" })
+    },
+
+    deleteProduct: async (_: any, args: any) => {
+      await Product.update({ _id: args.productId }, { deletedAt: new Date() })
+    },
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  cors: true,
+})
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -10,9 +84,13 @@ const AppDataSource = new DataSource({
   database: "postgres",
   synchronize: true,
   logging: true,
-  entities: [Quiz],
+  entities: [Product],
 })
 
 AppDataSource.initialize().then(() => {
-  console.log("연결성공")
+  console.log("DB연결성공")
+
+  server.listen(4000).then(({ url }) => {
+    console.log(`🚀 서버 연결 성공 ${url}`)
+  })
 })
